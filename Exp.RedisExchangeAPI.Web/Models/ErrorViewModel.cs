749dca0ec9d733744fd3035436cc3aef177f1a5e
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2022-11-23 01:53:42 +0300

    Added ExchangeAPI

---
+++ Exp.RedisExchangeAPI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exp.RedisExchangeAPI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
---
+++ Exp.RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace Exp.RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string? _redisHost;
+        private readonly string? _redisPort;
+        private ConnectionMultiplexer _redis;
+        public IDatabase? db { get; set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+
+        }
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
+
+
+
+
+
+
+
